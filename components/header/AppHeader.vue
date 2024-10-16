0edae901af149efc616c3adce5d953a3f3082f23
Pihkaal <pihkaal@proton.me>
2024-10-16 02:28:56 +0200

    feat(ui): fade in client only elements

--- components/header/AppHeader.vue
+++ components/header/AppHeader.vue
@@ -4,18 +4,35 @@
   >
     <h1 class="text-4xl font-bold">Simple QRCode Generator</h1>
 
-    <div class="flex gap-x-1">
-      <UButton
-        color="gray"
-        variant="ghost"
-        icon="i-uil-github"
-        aria-label="Github repo"
-        class="w-8 h-8"
-        target="_blank"
-        to="https://github.com/pihkaal/simple-qr"
-      />
+    <ClientOnly>
+      <div class="flex gap-x-1 animate-fadeIn">
+        <UButton
+          color="gray"
+          variant="ghost"
+          icon="i-uil-github"
+          aria-label="Github repo"
+          class="w-8 h-8"
+          target="_blank"
+          to="https://github.com/pihkaal/simple-qr"
+        />
 
-      <ThemeSwitcher />
-    </div>
+        <ThemeSwitcher />
+      </div>
+    </ClientOnly>
   </div>
 </template>
+
+<style scoped>
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.animate-fadeIn {
+  animation: fadeIn 200ms ease-in-out forwards;
+}
+</style>
